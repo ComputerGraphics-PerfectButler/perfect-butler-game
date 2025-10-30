f0c278262b9f09cf6cd1f23af4a304517d6be6ed
junhaww <chicowith5@gmail.com>
2025-10-30 16:48:46 +0900

    fix: edit directory
    fix: edit directory

--- PerfectButler/Assets/_Project/Scripts/GameSystem/GameManager.cs
+++ PerfectButler/Assets/_Project/Scripts/GameSystem/GameManager.cs
@@ -22,6 +22,6 @@ public class GameManager : MonoBehaviour
     
     void Start()
     {
-        Debug.Log("Perfect Butler Game Started!");
+        Debug.Log("MeowChoice Game Started!");
     }
 }
